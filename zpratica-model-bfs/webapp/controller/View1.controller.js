972c4da3b0117bb6710e10596a4ad00490bfcd52
Bruno Fernando da Silva <bruno.fernando@nttdata.com>
2024-11-08 18:34:11 +0000

    first commit

---
+++ zpratica-model-bfs/webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("mentoria.fiori.ka.zpraticamodelbfs.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ zpratica-model-bfs/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["mentoria/fiori/ka/zpraticamodelbfs/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
