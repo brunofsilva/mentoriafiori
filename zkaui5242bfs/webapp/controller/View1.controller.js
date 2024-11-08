eee099dc843028487d412535197f2545c10045a1
Bruno Fernando da Silva <bruno.fernando@nttdata.com>
2024-11-08 18:16:41 +0000

    i18n commit

--- zkaui5242bfs/webapp/controller/View1.controller.js
+++ zkaui5242bfs/webapp/controller/View1.controller.js
@@ -2,41 +2,66 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/Fragment",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/BindingMode"
+    "sap/m/MessageToast"
 ],
-    function (Controller, Fragment, JSONModel, BindingMode) {
+    function (Controller, Fragment, JSONModel, MessageToast) {
         "use strict";
 
         return Controller.extend("mentoria.fiori.ka.zkaui5242bfs.controller.View1", {
 
             onInit: function () {
-                var oModelJson = new JSONModel({ 
+                
+                var oModelJson = new JSONModel({
                     name: 'João',
                     showSecondName: true
                 })
                 this.getView().setModel(oModelJson, "model1");
 
-                var oModelJson2 = new JSONModel({ 
+                var oModelJson2 = new JSONModel({
                     name: 'Pedro'
                 })
                 this.getView().setModel(oModelJson2, "model2");
 
-                var oModelJson3 = new JSONModel();
-                oModelJson3.loadData("model/Products.json")
-                this.getView().setModel(oModelJson3, "model3");
                 
+                var oModelJson3 = new JSONModel();
+               
+                oModelJson3.loadData("/model/Products.json")
+
+                this.getView().setModel(oModelJson3, "model3");
+
                 var oModelJson4 = new JSONModel();
-                oModelJson4.loadData("model/Employees.json")
+                oModelJson4.loadData("/model/Employees.json")
                 this.getView().setModel(oModelJson4, "model4");
+
+                var oBubdle = this.getOwnerComponent().getModel("i18n").getResourceBundle(),
+                    msg = oBubdle.getText("msgInit", ["com", "sucesso"]);
+
+                MessageToast.show(msg);
 
             },
 
+            formatAlertStock: function (units) {
+                if (units > 0) {
+                    return ""
+                } else {
+                    return "Disponível em breve";
+                }
+            },
+
             onDataComboBoxChange: function (oEvent) {
+                //Pegando item selecionado no combobox
                 var oItem = oEvent.getParameter("selectedItem")
+
+                //Pegando o contexto da vinculação e depois o caminho dele (path)
+                //Preciso colocar o nome do model quando eu dou um nome para ele na vinculação do onInit
                 var sPath = oItem.getBindingContext("model4").getPath();
 
+                //Obtendo eferencia da lista
                 var oList = this.byId("listEmployees");
-                oList.bindElement({path: sPath, model: "model4"});
+
+                //Vinculando toda a lista ao caminho/contexto relativo ao item do combobox selecionado
+                //Preciso colocar o nome do model quando eu dou um nome para ele na vinculação do onInit
+                oList.bindElement({ path: sPath, model: "model4" });
             },
 
             onOpenDialog: function () {
